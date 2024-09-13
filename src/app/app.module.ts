ddd329ffe52703ec2390c21425d8afed0a6a94fa
Bansi Patel <bansip1997@gmail.com>
2024-09-13 14:05:59 +0530

    Assessment Submission

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
+// src/app/app.module.ts
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StringCalculatorComponent } from './pages/string-calculator/string-calculator.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, StringCalculatorComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
